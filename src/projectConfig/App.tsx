8036ab745d3d9b48b2fd576f85fa0feb660549c9
Julio Echeverri <julio.e.marulanda@gmail.com>
2022-12-04 08:21:21 -0500

    Added basic navigation structure

--- src/projectConfig/App.tsx
+++ src/projectConfig/App.tsx
@@ -1,21 +1,26 @@
 import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import store, { persistor } from '@redux/store';
-import Home from '@screens/Home';
+import AppNavigator from '@app/navigation/MainNavigator';
 import { SetupApi } from '@config/appSetup';
 
 import './src/config';
 
 const App = () => {
+
   useEffect(() => {
     SetupApi();
   }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Home />
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
       </PersistGate>
     </Provider>
   );
